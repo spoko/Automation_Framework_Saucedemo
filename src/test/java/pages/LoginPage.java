32cba8dbedefadf593eb935535138ea900b058eb
Ibraim Asanov <iasanov@nemetschek.bg>
2022-03-25 21:08:11 +0200

    Next

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,6 +18,19 @@ public class LoginPage {
     @FindBy(xpath = "//input[@value='Login']")
     private WebElement loginBtn;
 
+    @FindBy(xpath = "//*[text()='Epic sadface: Username and password do not match any user in this service']")
+    private WebElement genericErrorMessage;
+
+    @FindBy(xpath = "the other locator")
+    private WebElement usernameEmptyErrorMessage;
+
+    @FindBy(xpath = "the other locator")
+    private WebElement passwordEmptyErrorMessage;
+
+    @FindBy(xpath = "//button[@class='error-button']")
+    private WebElement xButtonOfLoginErroMessages;
+
+
     public LoginPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -32,5 +46,32 @@ public class LoginPage {
         loginBtn.click();
 
         return new ProductsPage(driver);
+    }
+
+    public void tryToLogin(String username, String password){
+        userNameInput.click();
+        userNameInput.sendKeys(username);
+
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+
+        loginBtn.click();
+
+//        try{
+//            WebElement productsMenu = driver.findElement(By.id("react-burger-menu-btn"));
+//        } catch (NoSuchElementException e){
+//            return false;
+//        }
+//        return true;
+    }
+
+    public boolean isLoginErrorMessageShown(){
+//        try {
+//            return genericErrorMessage.isDisplayed() || usernameEmptyErrorMessage.isDisplayed() || passwordEmptyErrorMessage.isDisplayed();
+//        }catch (NoSuchElementException e){
+//            //do some needed logic here
+//            return false;
+//        }
+        return xButtonOfLoginErroMessages.isDisplayed();
     }
 }
--- src/test/java/qa/automation/LoginTests.java
+++ src/test/java/qa/automation/LoginTests.java
@@ -35,17 +35,20 @@ public class LoginTests extends TestUtil {
     public void UnsuccessfulLogin(String userName, String password){
         //driver.get("https://www.saucedemo.com/");
 
-        WebElement username = driver.findElement(By.id("user-name"));
-        username.click();
-        username.sendKeys(userName);
+//        WebElement username = driver.findElement(By.id("user-name"));
+//        username.click();
+//        username.sendKeys(userName);
+//
+//        //find element using xpath and indexing the results
+//        WebElement passwordInput = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
+//        passwordInput.click();
+//        passwordInput.sendKeys(password);
+//
+//        WebElement loginBtn = driver.findElement(By.cssSelector("[value=Login]"));
+//        loginBtn.click();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.tryToLogin("wrong", "wrong");
 
-        //find element using xpath and indexing the results
-        WebElement passwordInput = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
-        passwordInput.click();
-        passwordInput.sendKeys(password);
-
-        WebElement loginBtn = driver.findElement(By.cssSelector("[value=Login]"));
-        loginBtn.click();
 
         WebElement errorLoginLabel = driver.findElement(By.xpath("//*[text()='Epic sadface: Username and password do not match any user in this service']"));
 
